ca8d0511a70f198ff81d2515cd83f58c0b69da4b
ncou <nicolas_courade@yahoo.fr>
2017-04-23 17:32:47 +0200

    format text

--- jquery.stepper.js
+++ jquery.stepper.js
@@ -5,169 +5,170 @@
 //
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;( function( $, window, document, undefined ) {
+;
+(function($, window, document, undefined) {
 
-	"use strict";
+  "use strict";
 
-	var pluginName = "stepper",
-		defaults = {
-			selectorProgressBar: '.stepper-progress',
-			selectorInputNumber: '.stepper-number',
-			classNameChanging: 'is-changing',
-			decimals: 0,
-			unit: '%',
-			initialValue: '',
-			min: 0,
-			max: 100,
-			stepSize: 1
-		};
+  var pluginName = "stepper",
+    defaults = {
+      selectorProgressBar: '.stepper-progress',
+      selectorInputNumber: '.stepper-number',
+      classNameChanging: 'is-changing',
+      decimals: 0,
+      unit: '%',
+      initialValue: '',
+      min: 0,
+      max: 100,
+      stepSize: 1
+    };
 
-	// The actual plugin constructor
-	function Plugin( element, options ) {
-		this.element = element;
+  // The actual plugin constructor
+  function Plugin(element, options) {
+    this.element = element;
 
-		this.settings = $.extend( {}, defaults, options) ;
+    this.settings = $.extend({}, defaults, options);
 
-		this._defaults = defaults;
-		this._name = pluginName;
+    this._defaults = defaults;
+    this._name = pluginName;
 
-		this.init();
-	}
+    this.init();
+  }
 
-	// Avoid Plugin.prototype conflicts
-	$.extend( Plugin.prototype, {
+  // Avoid Plugin.prototype conflicts
+  $.extend(Plugin.prototype, {
 
-		init: function () {
+    init: function() {
 
-			// local variable
-			this.curDown = false;
-			this.mouseDownX = 0;
-			this.mouseDownValue = 0;
+      // local variable
+      this.curDown = false;
+      this.mouseDownX = 0;
+      this.mouseDownValue = 0;
 
-			// Cache elements
-			this.$el = $(this.element);
-			this.$input = this.$el.find(this.settings.selectorInputNumber);
-			this.$progress = this.$el.find(this.settings.selectorProgressBar);
+      // Cache elements
+      this.$el = $(this.element);
+      this.$input = this.$el.find(this.settings.selectorInputNumber);
+      this.$progress = this.$el.find(this.settings.selectorProgressBar);
 
-			// init values
-			this.min = this.$input.attr('min') || this.settings.min;
-			this.max = this.$input.attr('max') || this.settings.max;
-      
+      // init values
+      this.min = this.$input.attr('min') || this.settings.min;
+      this.max = this.$input.attr('max') || this.settings.max;
+
       this.initialValue = this.$input.val() || (this.settings.initialValue !== "" ? this.settings.initialValue : this.max);
 
-			this.setValue(this.initialValue);
+      this.setValue(this.initialValue);
 
-			// Bind events
-			this.$input.on( 'keydown', this.onKeyPress.bind(this) );
-			this.$input.on( 'blur', this.onBlur.bind(this) );
-			this.$input.on( 'paste input', this.onChange.bind(this) );
-			this.$el.on( 'mousedown touchstart', this.onMouseDown.bind(this) );
-			$(document).on( 'mouseup touchend', this.onMouseUp.bind(this) );
-			$(document).on( 'mousemove touchmove', this.onMouseMove.bind(this) );
-		},
+      // Bind events
+      this.$input.on('keydown', this.onKeyPress.bind(this));
+      this.$input.on('blur', this.onBlur.bind(this));
+      this.$input.on('paste input', this.onChange.bind(this));
+      this.$el.on('mousedown touchstart', this.onMouseDown.bind(this));
+      $(document).on('mouseup touchend', this.onMouseUp.bind(this));
+      $(document).on('mousemove touchmove', this.onMouseMove.bind(this));
+    },
 
-		onMouseDown: function (e) {
+    onMouseDown: function(e) {
 
-			this.mouseDownX = e.clientX || e.originalEvent.touches[0].clientX;
-			this.mouseDownValue = this.getValue();
+      this.mouseDownX = e.clientX || e.originalEvent.touches[0].clientX;
+      this.mouseDownValue = this.getValue();
 
-			this._changeStart();
-		},
+      this._changeStart();
+    },
 
-		onMouseUp: function (e) {
+    onMouseUp: function(e) {
 
-			this._changeEnd();
+      this._changeEnd();
 
-			//return this;
-		},
+      //return this;
+    },
 
-		onMouseMove: function (e) {
+    onMouseMove: function(e) {
 
-			if (this.curDown === true) {
-				var t = (e.clientX || e.originalEvent.touches[0].clientX) - this.mouseDownX;
-				this.setValue(this.mouseDownValue + t * this.settings.stepSize);
-			}
-		},
+      if (this.curDown === true) {
+        var t = (e.clientX || e.originalEvent.touches[0].clientX) - this.mouseDownX;
+        this.setValue(this.mouseDownValue + t * this.settings.stepSize);
+      }
+    },
 
-		onChange: function (e) {
-		    this._updateProgress(this.getValue());
-		},
+    onChange: function(e) {
+      this._updateProgress(this.getValue());
+    },
 
-		onBlur: function (e) {
-			this._changeEnd();
-			this.setValue(this.getValue());
-		},
+    onBlur: function(e) {
+      this._changeEnd();
+      this.setValue(this.getValue());
+    },
 
-		onKeyPress: function (e) {
-			// key press == 'Enter' we exit the input field
-			if (e.keyCode === 13) {
-              this.$input.blur();
-            }
-		},
+    onKeyPress: function(e) {
+      // key press == 'Enter' we exit the input field
+      if (e.keyCode === 13) {
+        this.$input.blur();
+      }
+    },
 
-		getValue: function () {
-			return parseFloat(this.$input.val()) || 0;
-		},
+    getValue: function() {
+      return parseFloat(this.$input.val()) || 0;
+    },
 
-		setValue: function (amount) {
-			var value;
+    setValue: function(amount) {
+      var value;
 
-		    value = Math.max(Math.min(amount, this.max), this.min);
-		    value = this._roundValue(value);
+      value = Math.max(Math.min(amount, this.max), this.min);
+      value = this._roundValue(value);
 
-		    var n = value;
-		    n = n.toFixed(this.settings.decimals);
+      var n = value;
+      n = n.toFixed(this.settings.decimals);
 
-		    n += this.settings.unit;
-		    this.$input.val(n);
+      n += this.settings.unit;
+      this.$input.val(n);
 
-		    this._updateProgress(value);
-		},
+      this._updateProgress(value);
+    },
 
-		_updateProgress: function (v) {
-			var r = this._valueToPercent(v) / 100;
-		    this.$progress.css("transform", "scaleX(" + r + ")");
+    _updateProgress: function(v) {
+      var r = this._valueToPercent(v) / 100;
+      this.$progress.css("transform", "scaleX(" + r + ")");
 
-		    this.$input.trigger("change");
-		},
+      this.$input.trigger("change");
+    },
 
-		_percentToValue: function (v) {
-		    return this.min + v / 100 * (this.max - this.min);
-		},
+    _percentToValue: function(v) {
+      return this.min + v / 100 * (this.max - this.min);
+    },
 
-		_valueToPercent: function (v) {
-			var t = (v - this.min) / (this.max - this.min) * 100;
-			return Math.max(Math.min(t, 100), 0);
-		},
+    _valueToPercent: function(v) {
+      var t = (v - this.min) / (this.max - this.min) * 100;
+      return Math.max(Math.min(t, 100), 0);
+    },
 
-		_roundValue: function (v) {
-			var nbrDecimals = 2;
+    _roundValue: function(v) {
+      var nbrDecimals = 2;
 
-			var t = Math.pow(10, nbrDecimals);
-			return Math.round(v * t) / t
-		},
+      var t = Math.pow(10, nbrDecimals);
+      return Math.round(v * t) / t
+    },
 
-		_changeStart: function() {
-			this.curDown = true;
-			this.$el.addClass(this.settings.classNameChanging);
-		},
+    _changeStart: function() {
+      this.curDown = true;
+      this.$el.addClass(this.settings.classNameChanging);
+    },
 
-		_changeEnd: function() {
-			this.curDown = false;
-			this.$el.removeClass(this.settings.classNameChanging);
-		},
-		
-	} );
+    _changeEnd: function() {
+      this.curDown = false;
+      this.$el.removeClass(this.settings.classNameChanging);
+    },
 
-	// A really lightweight plugin wrapper around the constructor,
-	// preventing against multiple instantiations
-	$.fn[pluginName] = function ( options ) {
-		return this.each(function () {
-			if ( ! $.data(this, "plugin-" + pluginName)) {
-				$.data(this, "plugin-" + pluginName,
-					new Plugin( this, options ));
-				}
-		});
-	};
+  });
 
-} )( jQuery, window, document );
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function(options) {
+    return this.each(function() {
+      if (!$.data(this, "plugin-" + pluginName)) {
+        $.data(this, "plugin-" + pluginName,
+          new Plugin(this, options));
+      }
+    });
+  };
+
+})(jQuery, window, document);
